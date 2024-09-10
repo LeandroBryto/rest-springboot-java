da4d46d6c85a67f956966ead358ec141013d3b3d
leandro barreto <leandrobarreto.barreto321@gmail.com>
2024-09-10 18:44:52 -0300

    First Streps in java with spring boot 3

---
+++ 05_first/rest-springboot-java/rest-springboot-java/src/main/java/br/com/leandro/rest_springboot_java/RestSpringbootJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandro.rest_springboot_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringbootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringbootJavaApplication.class, args);
+	}
+
+}
---
+++ 05_first/rest-springboot-java/rest-springboot-java/src/test/java/br/com/leandro/rest_springboot_java/RestSpringbootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandro.rest_springboot_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
